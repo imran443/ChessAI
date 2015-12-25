30b0a5bc57f30ccf6887bdb488159c760015ba51
abubakar_saad <abubakar.saad.891@gmail.com>
2015-12-25 15:56:20 -0500

    temp changes

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,26 +1,32 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.border.*;
 
-public class Gui extends JPanel {
+public class Gui extends JPanel implements MouseListener, MouseMotionListener{
 	
-	
+	// board of buttons
 	JButton[][] board = new JButton[8][8];
+	// menubar
 	JToolBar tools = new JToolBar();
 	
+	// Button for menu bar
+	JButton new_game = new JButton("New Game");
+	JButton save = new JButton("Save");
+	JButton reset = new JButton("Reset");
 	
 	public Gui(){
 		// menu bar
 		setBorder(new EmptyBorder(15,15,15,15));
 		tools.setFloatable(false);
-		tools.add(new JButton("New Game"));
+		tools.add(new_game);
 		tools.addSeparator();
-		tools.add(new JButton("Save"));
+		tools.add(save);
 		tools.addSeparator();
-		tools.add(new JButton("Reset"));
+		tools.add(reset);
 		
 		// background
 		setBackground(Color.GRAY);
@@ -28,12 +34,10 @@ public class Gui extends JPanel {
 		
 		setLayout(new GridLayout(8, 8));
 		
-		
-		
-		
 		// chess board
 		for(int i=0; i<board.length; i++){
 			for(int j=0; j<board[i].length; j++){
+				// alternate button have black and white background
 				if((i%2 == 0 && j % 2 == 0) || (i%2 == 1 && j%2==1)){
 					JButton button = new JButton();
 					button.setBackground(Color.WHITE);
@@ -47,6 +51,51 @@ public class Gui extends JPanel {
 				}
 			}
 		}
+		
+		
+		// places pawns on chess board
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
