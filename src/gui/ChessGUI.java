006125bd5634c93e5059aeab33b74b52f75703f3
abubakar_saad <abubakar.saad.891@gmail.com>
2015-12-24 21:15:54 -0500

    Tool bar and border around the board

--- src/gui/ChessGUI.java
+++ src/gui/ChessGUI.java
@@ -1,6 +1,9 @@
 package gui;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.*;
 
 public class ChessGUI {
 	public static void main(String[] args){
@@ -8,7 +11,9 @@ public class ChessGUI {
 		frame.setSize(1000, 1000);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Gui gui_button = new Gui();
-		frame.getContentPane().add(gui_button);;
+		frame.getContentPane().add(gui_button.tools, BorderLayout.PAGE_START);
+		frame.getContentPane().add(gui_button, BorderLayout.CENTER);
+		
 		frame.setVisible(true);
 	}
 }
--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -3,18 +3,35 @@ package gui;
 import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class Gui extends JPanel {
 	
 	
 	JButton[][] board = new JButton[8][8];
+	JToolBar tools = new JToolBar();
 	
 	
 	public Gui(){
-		setBackground(Color.CYAN);
+		// menu bar
+		setBorder(new EmptyBorder(15,15,15,15));
+		tools.setFloatable(false);
+		tools.add(new JButton("New Game"));
+		tools.addSeparator();
+		tools.add(new JButton("Save"));
+		tools.addSeparator();
+		tools.add(new JButton("Reset"));
+		
+		// background
+		setBackground(Color.GRAY);
+		setSize(600,600);
+		
 		setLayout(new GridLayout(8, 8));
 		
 		
+		
+		
+		// chess board
 		for(int i=0; i<board.length; i++){
 			for(int j=0; j<board[i].length; j++){
 				if((i%2 == 0 && j % 2 == 0) || (i%2 == 1 && j%2==1)){
@@ -32,6 +49,8 @@ public class Gui extends JPanel {
 		}
 		
 	}
+	
+	
 	
 	
 	
