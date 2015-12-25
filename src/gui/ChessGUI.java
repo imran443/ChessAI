67ebd71d9314356c1d72011f29aeec6d6fc9940a
imran443 <qureshi.443@gmail.com>
2015-12-24 19:16:41 -0500

    Moved code from ChessGUI to Main

--- src/gui/ChessGUI.java
+++ src/gui/ChessGUI.java
@@ -3,12 +3,5 @@ package gui;
 import javax.swing.JFrame;
 
 public class ChessGUI {
-	public static void main(String[] args){
-		JFrame frame = new JFrame("Chess");
-		frame.setSize(1000, 1000);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Gui gui_button = new Gui();
-		frame.getContentPane().add(gui_button);;
-		frame.setVisible(true);
-	}
+	
 }
