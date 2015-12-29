64f84379f559820e31b6bcde6e980a8fd3a986ed
abubakar_saad <abubakar.saad.891@gmail.com>
2015-12-29 15:42:25 -0500

    captures working

--- src/Algorithm/Pawn.java
+++ src/Algorithm/Pawn.java
@@ -21,49 +21,51 @@ public class Pawn extends Piece{
 				moves.add(pMove);
 			}
 			//If the two spots in front of it are empty and it is in the original position it can jump two spots
-			if(chessBoard[sourceX+2][sourceY].equals(" ") && chessBoard[sourceX+1][sourceY].equals(" ") && sourceX==1){
-				newX = sourceX + 2;
-				pMove = newX + " " + sourceY;
-				moves.add(pMove);
-			}
+			if(sourceX == 1)
+				if(chessBoard[sourceX+2][sourceY].equals(" ") && chessBoard[sourceX+1][sourceY].equals(" ")){
+					newX = sourceX + 2;
+					pMove = newX + " " + sourceY;
+					moves.add(pMove);
+				}
 			//Can capture a White piece if itself is black piece in the diagonal
 			if(sourceY+1 < 8)
-			if(chessBoard[sourceX+1][sourceY+1].equals("P") && pieceColor == Gui.BLACK){
-				newX = sourceX + 1;
-				newY = sourceY + 1;
-				pMove = newX + " " + newY;
-				moves.add(pMove);
-			}
+				if(Character.isUpperCase(chessBoard[sourceX+1][sourceY+1].charAt(0)) && pieceColor == Gui.BLACK){
+					newX = sourceX + 1;
+					newY = sourceY + 1;
+					pMove = newX + " " + newY;
+					moves.add(pMove);
+				}
 			if(sourceY-1 >=0)
-			if(chessBoard[sourceX+1][sourceY-1].equals("P") && pieceColor == Gui.BLACK){
-				newX = sourceX + 1;
-				newY = sourceY - 1;
-				pMove = newX + " " + newY;
-				moves.add(pMove);
-			}
+				if(Character.isUpperCase(chessBoard[sourceX+1][sourceY-1].charAt(0)) && pieceColor == Gui.BLACK){
+					newX = sourceX + 1;
+					newY = sourceY - 1;
+					pMove = newX + " " + newY;
+					moves.add(pMove);
+				}
 		}
-		if(pieceColor == Gui.WHITE){
+		else if(pieceColor == Gui.WHITE){
 			if(chessBoard[sourceX-1][sourceY].equals(" ")){
 				newX = sourceX - 1;
 				pMove = newX + " " + sourceY;
 				moves.add(pMove);
 			}
 			//If the two spots in front of it are empty and it is in the original position it can jump two spots
-			if(chessBoard[sourceX - 2][sourceY].equals(" ") && chessBoard[sourceX - 1][sourceY].equals(" ") && sourceX==6){
-				newX = sourceX - 2;
-				pMove = newX + " " + sourceY;
-				moves.add(pMove);
-			}
+			if(sourceX==6)
+				if(chessBoard[sourceX - 2][sourceY].equals(" ") && chessBoard[sourceX - 1][sourceY].equals(" ")){
+					newX = sourceX - 2;
+					pMove = newX + " " + sourceY;
+					moves.add(pMove);
+				}
 			//Can capture a white piece if itself is black piece in the diagonal
 			if(sourceY-1 >= 0)
-			if(chessBoard[sourceX - 1][sourceY - 1].equals("p") && pieceColor == Gui.WHITE){
-				newX = sourceX - 1;
-				newY = sourceY - 1;
-				pMove = newX + " " + newY;
-				moves.add(pMove);
-			}
+				if(Character.isLowerCase(chessBoard[sourceX - 1][sourceY - 1].charAt(0)) && pieceColor == Gui.WHITE){
+					newX = sourceX - 1;
+					newY = sourceY - 1;
+					pMove = newX + " " + newY;
+					moves.add(pMove);
+				}
 			if(sourceY+1 <8){
-				if(chessBoard[sourceX - 1][sourceY + 1].equals("p") && pieceColor == Gui.WHITE){
+				if(Character.isLowerCase(chessBoard[sourceX - 1][sourceY + 1].charAt(0)) && pieceColor == Gui.WHITE){
 					newX = sourceX - 1;
 					newY = sourceY + 1;
 					pMove = newX + " " + newY;
